128ebb5407557d32de1f0dae6260c42300ee30de
erlan artykbaev <erik.2405@mail.ru>
2020-10-09 10:35:57 +0600

    set routes get and post to career

--- routes/career.routes.js
+++ routes/career.routes.js
@@ -1,7 +1,26 @@
 const {Router} = require('express')
+const Career = require('../models/Career')
 
 const router = Router()
 
-router.get('/career', () => {
-
+// api/careers/
+router.get('/', (req, res) => {
+	Career.find({}, (err, result) => {
+		if(err){
+			res.status(400).json({message: "cant find career"})
+		}
+		res.send(result)
+	})
 })
+
+// api/careers/create/
+router.post('/create', async (req, res) => {
+	const {title, salary, stackTechnology, requirement, more} = await res.body
+
+	const newCareer = new Career({title, salary,stackTechnology, requirement, more})
+	await newCareer.save()
+
+	res.status(200).json({message: 'career created'})
+})
+
+module.exports = router
